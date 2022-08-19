dfacc0edc273c9a9238d00c8f993c11d3a91935e
HuangZiming0830 <18811066672@163.com>
2022-08-19 17:10:45 +0800

    Site updated: 2022-08-19 17:10:30

--- js/matery.js
+++ js/matery.js
@@ -35,6 +35,17 @@ $(function () {
         $('#' + targetId).width(w);
     };
 
+    /* insert function */
+    //黑夜模式提醒开启功能
+    setTimeout(function () {
+        if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+            let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用黑夜模式阅读能够减轻视觉疲劳。</span>'
+            M.toast({ html: toastHTML })
+        }
+    }, 2000)
+
+    /* insert function */
+
     /**
      * 修复footer部分的位置，使得在内容比较少时，footer也会在底部.
      */
